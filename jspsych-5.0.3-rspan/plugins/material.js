ba646adca011669a32e559eea219259256a8fdc1
seasense <paultisina@msn.com>
2019-04-30 10:39:57 +0200

    task update

---
+++ jspsych-5.0.3-rspan/plugins/material.js
@@ -0,0 +1,334 @@
+/////////
+///////// Material / Sentences
+
+
+
+var p_sentence1 = {
+	S: "Die G&#xE4;rtnerin klettert auf den Baum und schneidet die Laterne.",
+	R: 0
+};
+var p_sentence2 = {
+	S: "Der Dachs schl&#xE4;ft in seinem Bau und f&#XFC;hlt sich wohl.",
+	R: 1
+};
+var p_sentence3 = {
+	S: "Im Winter kann man an der K&#XFC;ste sehr g&#XFC;nstige Zimmer einfrieren.",
+	R: 0
+};
+var p_sentence4 = {
+	S: "Andreas wurde vom Polizisten angehalten, weil er die Kreuzung bei rot &#XFC;berquert hatte.",
+	R: 1
+};
+var p_sentence5 = {
+	S: "Die Leute in unserer Stadt sind in der Weihnachtszeit viel spendabler und fr&#xF6;hlicher.",
+	R: 1
+};
+var p_sentence6 = {
+	S: "Wenn die letzten Pr&#xFC;fungen vorbei sind, k&#xF6;nnen wir eine wohlverdiente Pause kochen.",
+	R: 0
+};
+var p_sentence7 = {
+	S: "Nach einem langen Arbeitstag geht Peter oft in die Kneipe, um zu entspannen.",
+	R: 1
+};
+var p_sentence8 = {
+	S: "Eleonore hat den ganzen Tag damit verbracht, den Aufsatz zu planieren.",
+	R: 0
+};
+var sentences_and_responses_practice = [p_sentence1, p_sentence2, p_sentence3, p_sentence4, p_sentence5, p_sentence6, p_sentence7, p_sentence8];
+
+
+
+
+// The first 30 sentences are meaningul, i.e., R: 1
+var sentence1 = {
+	S: "Egal wie oft wir mit ihm sprechen, er wird sich nie &#xE4;ndern.",
+	R: 1
+};
+var sentence2 = {
+	S: "Der Kl&#xE4;ger verlor den Fall, weil er nicht auf Fakten beruhte.",
+	R: 1
+};
+var sentence3 = {
+	S: "Ich ertappe mich ab und zu, wie ich dasitze und meine Gedanken schweifen lasse.",
+	R: 1
+};
+var sentence4 = {
+	S: "Wir waren f&#xFC;nfzig Minuten auf dem Meer, als wir das Land nicht mehr sehen konnten.",
+	R: 1
+};
+var sentence5 = {
+	S: "W&#xE4;hrend des ganzen Martyriums schienen die Geiseln nie die Hoffnung verloren zu haben.",
+	R: 1
+};
+var sentence6 = {
+	S: "Paul hat H&#xF6;henangst und weigert sich, mit dem Flugzeug zu fliegen.",
+	R: 1
+};
+var sentence7 = {
+	S: "Der junge Teilnehmer hielt seine Augen geschlossen, bis er wieder schauen durfte.",
+	R: 1
+};
+var sentence8 = {
+	S: "Die meisten Menschen, die ich kene, sind besorgt dar&#xFC;ber, ihr Gewicht zu kontrollieren.",
+	R: 1
+};
+var sentence9 = {
+	S: "Wenn Laura einkaufen geht, sucht sie immer nach den g&#xFC;nstigsten Produkten.",
+	R: 1
+};
+var sentence10 = {
+	S: "Wenn ich morgens aufstehe, f&#xFC;ttere ich als Erstes meinen Hund.",
+	R: 1
+};
+var sentence11 = {
+	S: "Nachdem ich beim Spiel so viel geschrien hatte, wusste ich, dass meine Stimme angeschlagen sein w&#xFC;rde.",
+	R: 1
+};
+var sentence12 = {
+	S: "Der Koch holte mit der Kelle aus und traf den Kellner an der Schulter.",
+	R: 1
+};
+var sentence13 = {
+	S: "Man kann doch nicht jedes Mal Socken zu Weihnachten verschenken.",
+	R: 1
+};
+var sentence14 = {
+	S: "Viele Menschen w&#xFC;rden best&#xE4;tigen, dass blau eine sch&#xF6;ne Farbe f&#xFC;r ein Fahrrad ist.",
+	R: 1
+};
+var sentence15 = {
+	S: "Der Kunde bemerkte, dass etwas nicht stimmte mit der Verk&#xE4;uferin.",
+	R: 1
+};
+var sentence16 = {
+	S: "Am n&#xE4;chsten Morgen war auf einmal der Transporter mit der ganzen Ware verschwunden.",
+	R: 1
+};
+var sentence17 = {
+	S: "Das Wetter war &#xFC;beraus launisch an diesem Nachmittag im September.",
+	R: 1
+};
+var sentence18 = {
+	S: "Marie sollte am neuen Einkaufszentrum anhalten, um ein paar Dinge zu besorgen.",
+	R: 1
+};
+var sentence19 = {
+	S: "Wenn es kalt ist, will meine Mutter immer, dass ich eine M&#xFC;tze trage.",
+	R: 1
+};
+var sentence20 = {
+	S: "Alle Eltern hoffen, dass ihre Liebsten sp&#xE4;ter einmal zufrieden sein werden.",
+	R: 1
+};
+var sentence21 = {
+	S: "Als Tom und Anna nach Kanada zogen, veranstalteten ihre Freunde einen großen Flohmarkt.",
+	R: 1
+};
+var sentence22 = {
+	S: "Im Herbst arbeiten meine Schwester und ich gerne zusammen im Garten.",
+	R: 1
+};
+var sentence23 = {
+	S: "Martins Tochter hat gestern Vormittag in der Kirche eine k&#xF6;stliche Torte gemacht.",
+	R: 1
+};
+var sentence24 = {
+	S: "Das Reh bemerkte den J&#xE4;ger nicht und lief direkt in seine Schusslinie.",
+	R: 1
+};
+var sentence25 = {
+	S: "Da es das letzte Spiel war, konnten sie nur schwer mit der Niederlage umgehen.",
+	R: 1
+};
+var sentence26 = {
+	S: "Da Sarah immer fr&#xFC;h zum Dienst kommt, bekommt sie meistens einen guten Parkplatz.",
+	R: 1
+};
+var sentence27 = {
+	S: "Das einzige M&#xF6;belst&#xFC;ck, das Stefan in seiner ersten Wohnung hatte, war sein Wasserbett.",
+	R: 1
+};
+var sentence28 = {
+	S: "Letztes Jahr musste Michael nachsitzen, weil er im Flur gerannt war.",
+	R: 1
+};
+var sentence29 = {
+	S: "Die riesigen Wolken bedeckten den morgendlichen Himmel und es fing an zu regnen.",
+	R: 1
+};
+var sentence30 = {
+	S: "Nach der ersten Verabredung wusste ich, dass Lindas Schwester einfach nicht mein Typ war.",
+	R: 1
+};
+var sentence31 = {
+	S: "Die einzige M&#xFC;hsal dieser Woche bestand darin, die Sorge zu putzen.",
+	R: 0,
+	ID: 31
+};
+var sentence32 = {
+	S: "Das Erste, was Simon sich kochen w&#xFC;rde, w&#xE4;re der italienische Teppich.",
+	R: 0,
+	ID: 32
+};
+var sentence33 = {
+	S: "Manche Pflanzen brauchen nur sehr wenig Kalender, um zu &#xFC;berleben.",
+	R: 0,
+	ID: 33
+};
+var sentence34 = {
+	S: "Es war nicht zu &#xFC;bersehen, dass sie ein Auge auf ihn gelesen hatte.",
+	R: 0,
+	ID: 34
+};
+var sentence35 = {
+	S: "Als er vom Baum auf den Boden fiel, brach sich Jan den Spielstand.",
+	R: 0,
+	ID: 35
+};
+var sentence36 = {
+	S: "Die meisten Leute sind sich einig, dass Montag der schlimmste Tag des Klaviers ist.",
+	R: 0,
+	ID: 36
+};
+var sentence37 = {
+	S: "An warmen, sonnigen Nachmittagen gehe ich gern im Park eislaufen.",
+	R: 0,
+	ID: 37
+};
+var sentence38 = {
+	S: "Mit großem Ehrgeiz überwand er alle Hindernisse und kochte das Rennen.",
+	R: 0,
+	ID: 38
+};
+var sentence39 = {
+	S: "Meine Mutter sagt mir immer, dass es nicht höflich ist zu fliegen.",
+	R: 0,
+	ID: 39
+};
+var sentence40 = {
+	S: "Das Erziehen von Kindern erfordert viel Standhaftigkeit und Staub.",
+	R: 0,
+	ID: 40
+};
+
+var sentences_and_responses = [sentence1, sentence2, sentence3, sentence4, sentence5, sentence6, sentence7, sentence8, sentence9, sentence10,
+								sentence11, sentence12, sentence13, sentence14, sentence15, sentence16, sentence17, sentence18, sentence19, sentence20,
+								sentence21, sentence22, sentence23, sentence24, sentence25, sentence26, sentence27, sentence28, sentence29, sentence30,
+								sentence31, sentence32, sentence33, sentence34, sentence35, sentence36, sentence37, sentence38, sentence39, sentence40
+								];
+
+/*
+Ein Mensch sollte wirklich nie aufgrund seiner Herkunft diskriminiert werden.
+
+Die Limonadenspieler entschlossen sich, zwei von drei Sätzen zu spielen.
+
+
+// ä &#xE4; Ä &#xC4; 
+// ö &#xF6; Ö &#xD6;
+// ü &#xFC; Ü &#xDC;
+// ß &#xDF;
+
+
+var sentence41 = {
+	S: "...",
+	R: 0,
+	ID: 41
+};
+var sentence42 = {
+	S: "",
+	R: 0
+};
+var sentence43 = {
+	S: "",
+	R: 0
+};
+var sentence44 = {
+	S: "",
+	R: 0
+};
+var sentence45 = {
+	S: "",
+	R: 0
+};
+var sentence46 = {
+	S: "",
+	R: 0
+};
+var sentence47 = {
+	S: "",
+	R: 0
+};
+var sentence48 = {
+	S: "",
+	R: 0
+};
+var sentence49 = {
+	S: "",
+	R: 0
+};
+var sentence50 = {
+	S: "",
+	R: 0
+};
+var sentence51 = {
+	S: "",
+	R: 0
+};
+var sentence52 = {
+	S: "",
+	R: 0
+};
+var sentence53 = {
+	S: "",
+	R: 0
+};
+var sentence54 = {
+	S: "",
+	R: 0
+};
+var sentence55 = {
+	S: "",
+	R: 0
+};
+var sentence56 = {
+	S: "",
+	R: 0
+};
+var sentence57 = {
+	S: "",
+	R: 0
+};
+var sentence58 = {
+	S: "",
+	R: 0
+};
+var sentence59 = {
+	S: "",
+	R: 0
+};
+var sentence60 = {
+	S: "",
+	R: 0
+};
+
+
+Sobald ich mit diesem Neid fertig bin, gehe ich nach Hause.
+Susann öffnete ihr Portemonnaie und sah, dass sie kein Geld bei sich hatte.
+Julia wollte einen Garten in ihrem Hinterhof, aber der Boden war zu lehmig.
+Ich hatte keine Ahnung, warum meine Freundin sauer auf mich war.
+Nachdem ich mich sehr warm angezogen hatte, hörte ich endlich auf zu frieren.
+Als ich aus dem Fenster blickte, war die ganze Straße in Gestank gehüllt
+Während der gesamten Zeit in Frankreich blies der Wind unwahrscheinlich kalt von Norden.
+Als sie aus dem Geschäft kam, wurde sie von einem Passanten getrunken.
+Der Spieler traf das Tor besser, als ich es jemals erwartet hätte.
+Tina hörte auf, sich mit dem Licht zu treffen, als sie erfuhr, dass er verheiratet war.
+Ich sagte meinen Schülern, sie würden eine Überraschung bekommen, wenn sie orange wären.
+Tom war so müde vom Lernen, dass er keine einzige Seite mehr lesen konnte.
+Obwohl Jonas manchmal sarkastisch ist, kann er doch auch sehr lieb sein.
+Caro wird ihren Schuh fragen, wie viel der Flug nach Mexiko kostet.
+Der Zucker konnte nicht glauben, dass man ihm so ein tolles Angebot unterbreitete.
+
+
+var sentences_and_responses_practice = [sentence1, sentence2, sentence3, sentence4, sentence5, sentence6];
+*/
